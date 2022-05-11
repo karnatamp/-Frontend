6d5ec283377696ff4b9e39efb61d06d27ed3401d
praveen <karnatamp@gmail.com>
2022-05-11 11:01:54 +0200

    first commit

---
+++ TicTacToe/js/main.js
@@ -0,0 +1,17 @@
+console("how are you")
+function smile() {
+    var a;
+    a = document.getElementById("div1");
+    a.innerHTML = "&#xf118;";
+    setTimeout(function () {
+        a.innerHTML = "&#xf11a;";
+      }, 1000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf119;";
+      }, 2000);
+    setTimeout(function () {
+        a.innerHTML = "&#xf11a;";
+      }, 3000);
+  }
+  smile();
+  setInterval(smile, 4000);
